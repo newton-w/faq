cb88b8ba9545a34f6e26f55cd7c9240015e1a43c
newton-w <newtonwarui80@gmail.com>
2023-12-21 15:16:45 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -55,10 +55,10 @@ function App() {
               <h1 className=' font-work_sans text-lg font-bold text-darkpurple'>FAQs</h1>
             </div>
 
-            <div className='-translate-y-8'>
+            <main className='-translate-y-8 w-full'>
               <div className={classNames('group py-6 border-b border-lightpink  duration-300 h-20 relative cursor-pointer ', { ' h-52 md:h-40 ': one === false })} onClick={FirstClick}>
                 <div className=' flex items-center justify-between'>
-                  <p className='font-bold text-darkpurple w-72 md:w-full group-hover:text-purple text-sm md:text-base'>what is Frontend Mentor, and how will it help me?</p>
+                  <p className='font-bold text-darkpurple w-52 md:w-full group-hover:text-purple text-sm md:text-base'>what is Frontend Mentor, and how will it help me?</p>
                   <div className={classNames('z-50 cursor-pointer  relative h-6 w-6 flex items-center justify-center rounded-full duration-300 bg-darkpurple group-hover:bg-purple', { 'rotate-180 ': one === true })} onClick={FirstClick}>
                     <div className='bg-white w-3 h-[2px] '></div>
                     <div className={classNames('bg-white w-3 h-[2px] absolute', { 'rotate-90': one === true })}></div>
@@ -69,7 +69,7 @@ function App() {
 
               <div className={classNames('group py-6 border-b border-lightpink  duration-300 h-20 relative cursor-pointer', { ' h-52 md:h-40': two === false })} onClick={SecondClick}>
                 <div className=' flex items-center justify-between'>
-                  <p className='font-bold text-darkpurple w-72 md:w-full group-hover:text-purple text-sm md:text-base'>Is Frontend Mentor free?</p>
+                  <p className='font-bold text-darkpurple w-52 md:w-full group-hover:text-purple text-sm md:text-base'>Is Frontend Mentor free?</p>
                   <div className={classNames('z-50 cursor-pointer  relative h-6 w-6 flex items-center justify-center rounded-full duration-300 bg-darkpurple group-hover:bg-purple', { 'rotate-180 ': two === true })} onClick={SecondClick}>
                     <div className='bg-white w-3 h-[2px] '></div>
                     <div className={classNames('bg-white w-3 h-[2px] absolute', { 'rotate-90': two === true })}></div>
@@ -80,26 +80,26 @@ function App() {
 
               <div className={classNames('group py-6 border-b border-lightpink  duration-300 h-20 relative cursor-pointer', { ' h-52 md:h-40': three === false })} onClick={ThirdClick}>
                 <div className=' flex items-center justify-between'>
-                  <p className='font-bold text-darkpurple w-72 md:w-full group-hover:text-purple text-sm md:text-base'>can I use Frontend Mentor projects on my portfolio?</p>
+                  <p className='font-bold text-darkpurple w-52 md:w-full group-hover:text-purple text-sm md:text-base'>can I use Frontend Mentor projects on my portfolio?</p>
                   <div className={classNames('z-50 cursor-pointer  relative h-6 w-6 flex items-center justify-center rounded-full duration-300 bg-darkpurple group-hover:bg-purple', { 'rotate-180 ': three === true })} onClick={SecondClick}>
                     <div className='bg-white w-3 h-[2px] '></div>
                     <div className={classNames('bg-white w-3 h-[2px] absolute', { 'rotate-90': three === true })}></div>
                   </div>
                 </div>
-                <p className={classNames('absolute text-grayishpurple pt-6 -translate-y-20 duration-300 opacity-0 text-sm md:text-base', { 'translate-y-0 opacity-100': three === false })}>Frontend Mentor offers free access to it's platform. While there is an additional premium features or content, the core platform and many of its resources are available for free.</p>
+                <p className={classNames('absolute text-grayishpurple pt-6 -translate-y-20 duration-300 opacity-0 text-sm md:text-base', { 'translate-y-0 opacity-100': three === false })}>Absolutely! They provide practical challenges that you can use to demonstrate your proficiency in front-end development.</p>
               </div>
 
               <div className={classNames('group py-6 duration-300 h-20 relative cursor-pointer', { ' h-52 md:h-40': four === false })} onClick={FourthClick}>
                 <div className=' flex items-center justify-between'>
-                  <p className='font-bold text-darkpurple w-72 md:w-full group-hover:text-purple text-sm md:text-base'>How can I get help if I'm stuck on a challenge?</p>
+                  <p className='font-bold text-darkpurple w-52 md:w-full group-hover:text-purple text-sm md:text-base'>How can I get help if I'm stuck on a challenge?</p>
                   <div className={classNames('z-50 cursor-pointer  relative h-6 w-6 flex items-center justify-center rounded-full duration-300 bg-darkpurple group-hover:bg-purple', { 'rotate-180 ': four === true })} onClick={SecondClick}>
                     <div className='bg-white w-3 h-[2px] '></div>
                     <div className={classNames('bg-white w-3 h-[2px] absolute', { 'rotate-90': four === true })}></div>
                   </div>
                 </div>
-                <p className={classNames('absolute text-grayishpurple pt-6 -translate-y-20 duration-300 opacity-0 text-sm md:text-base', { 'translate-y-0 opacity-100': four === false })}>Frontend Mentor offers free access to it's platform. While there is an additional premium features or content, the core platform and many of its resources are available for free.</p>
+                <p className={classNames('absolute text-grayishpurple pt-6 -translate-y-20 duration-300 opacity-0 text-sm md:text-base', { 'translate-y-0 opacity-100': four === false })}>Frontend Mentor has a community forum where you can ask questions and seek help from other developers who have completed or are working on the same challenges.</p>
               </div>
-            </div>
+            </main>
           </div>
         </div>
       </div >
